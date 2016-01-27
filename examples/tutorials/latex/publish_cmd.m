02c37ad0a064f3936d826da720ffe68a0818f658
Alexandre Donze <donze@eecs.berkeley.edu>
2016-01-27 21:53:06 +0100

    tutorial with publish script

---
+++ examples/tutorials/latex/publish_cmd.m
@@ -0,0 +1,10 @@
+%% 
+% To generate latex code, run the following:
+addpath ..
+%publish('tutorial_open_loop',struct('format','latex','stylesheet','matlab2latex-pretty.xsl', 'outputDir', '.'))
+%publish('tutorial_closed_loop',struct('format','latex','stylesheet','matlab2latex-pretty.xsl', 'outputDir', '.'))
+publish('tutorial1',struct('format','latex','stylesheet','matlab2latex-pretty.xsl', 'outputDir', '.'))
+
+% or use the publish button with the same button
+
+
